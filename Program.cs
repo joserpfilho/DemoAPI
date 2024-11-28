032b97aea22bbe28f9f6d1b20a30be0870c6674b
José <msreynaldojose@gmail.com>
2024-11-27 21:34:55 -0300

    Testing Scalar

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("Demo API")
+            .WithTheme(ScalarTheme.BluePlanet)
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+
+    });
 }
 
 app.UseHttpsRedirection();
